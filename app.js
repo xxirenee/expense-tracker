ca58e37421bb11837428474a9f438afab9b0f054
xxirenee <charice0709@gmail.com>
2022-11-13 23:12:09 +0800

    feat:add index.hbs & main.hbs

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Record = require('./models/record')
+
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 require('./config/mongoose')
 mongoose.connect(process.env.MONGODB_URI)
+const app = express()
+const port = 3000
 
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.send('This project is built with Express')
+  Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
