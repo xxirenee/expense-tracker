a8ee97d3461a062065508fe29644dec5e529537d
xxirenee <charice0709@gmail.com>
2022-11-19 23:44:47 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const Record = require('./models/record')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -15,6 +16,7 @@ const port = process.env.PORT
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   Record.find()
@@ -26,6 +28,16 @@ app.get('/', (req, res) => {
 app.get('/records/new', (req, res) => {
   return res.render('new')
 })
+
+app.post('/', async(req, res) => {
+    const { name, date, categoryId, amount } = req.body
+    try {
+      await Record.create({ name, date, categoryId, amount })
+      res.redirect('/')
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
 app.get('/records/edit', (req, res) => {
   return res.render('edit')
