aef0427abdd8e587be257c0241c14caa661c6825
xxirenee <charice0709@gmail.com>
2022-11-20 21:05:10 +0800

    feat:add edit & delete function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const dayjs = require('dayjs')
 
 //新增
 router.get('/new', (req, res) => {
@@ -15,14 +16,17 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   const { name, date, category, amount } = req.body
-
-  Category.find({})
+  const errors = []
+  if (!name || !date || !category || !amount) {
+    errors.push({ message: '所有欄位都要填！' })
+  }
+  if (errors.length !== 0) {
+    return Category.find({})
       .lean()
-      .sort({ _id: 'asc' })
       .then(categories => {
-        categories.forEach(item => {
-          if (item._id.toString() === category) {
-            item.selected = 'selected'
+        categories.forEach(data => {
+          if (data._id.toString() === category) {
+            data.selected = true
           }
         })
         res.render('new', {
@@ -32,23 +36,74 @@ router.post('/', (req, res) => {
           amount
         })
       })
-
-
-  Record.create({
+  }
+  return Record.create({
     name,
     date,
-    categoryId: category,
     amount,
-
-
+    categoryId: category,
   })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
 
 
-router.get('/edit', (req, res) => {
-  return res.render('edit')
+router.get('/:id/edit', (req, res) => {
+  const _id = req.params.id
+
+
+  Record.findOne({ _id })
+    .populate('categoryId')
+    .lean()
+    .then(record => {
+      record.date = record.date.toISOString().slice(0, 10)
+      Category.find({ _id: { $ne: record.categoryId._id } })
+        .lean()
+        .sort({ _id: 'asc' })
+        .then(categories => res.render('edit', { record, categories }))
+        .catch(error => console.error(error))
+    })
+    .catch(error => console.error(error))
+})
+
+// 編輯
+router.put('/:id', (req, res) => {
+
+  const _id = req.params.id
+
+  const { name, date, category, amount } = req.body
+  const errors = []
+  if (!name || !date || category === '' || !amount) {
+    errors.push({ message: '所有欄位都是必填！' })
+  }
+  if (errors.length !== 0) {
+    return Category.find({})
+      .lean()
+      .then(categories => {
+        categories.forEach(data => {
+          if (data._id.toString() === category) {
+            data.selected = true
+          }
+        })
+
+        return res.render('edit', { categories, record: req.body })
+      })
+      .catch(error => console.error(error))
+  }
+
+  Record.findByIdAndUpdate({ _id }, { name, date, categoryId: category, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+}
+)
+
+
+router.delete('/:id', (req, res) => {
+
+  const _id = req.params.id
+  Record.findByIdAndDelete({ _id })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
